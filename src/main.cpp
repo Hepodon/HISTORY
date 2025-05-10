91daa91340acaa2380413a04fea29a0546effec6
Hepdon <brandonlewis0120@gmail.com>
2025-05-10 16:49:32 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "pros/screen.hpp"
+
+using namespace pros;
+
+void initialize() { lvgl_init(); }
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {}
